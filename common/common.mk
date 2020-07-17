62da279f10de590541ffccd4fa9714fc822e033e
Melodyn <samelodyn@gmail.com>
2020-07-17 16:25:30 +0300

    refactoring compose commands

--- common/common.mk
+++ common/common.mk
@@ -1,28 +1,11 @@
-compose-sut:
-	docker-compose -f docker-compose.test.yml run sut
-
-compose-code-lint:
-	docker-compose run exercises make code-lint
-
-compose-description-lint:
-	docker-compose run exercises make description-lint
-
-compose-schema-validate:
-	docker-compose run exercises make schema-validate
-
-compose-bash:
-	docker-compose run exercises bash
+check: description-lint code-lint schema-validate test
 
 description-lint:
 	yamllint modules
 
-compose-test:
-	docker-compose run exercises make test
-
 test:
 	@(for i in $$(find modules/** -type f -name Makefile); do make test -C $$(dirname $$i) || exit 1; done)
 
-check: description-lint code-lint schema-validate test
 
 SUBDIRS := $(wildcard modules/**/*/.)
 
